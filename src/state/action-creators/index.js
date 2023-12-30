0d26b8311d9f47bba51cc62f9c0e39bade38a15a
hammahutz <62766637+hammahutz@users.noreply.github.com>
2023-12-30 11:37:20 +0100

    Clean up

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -7,23 +7,3 @@ const actionCreators = {
 }
 
 export default actionCreators;
-
-// export const depositMoney = (amount) => {
-//   return (dispatch) => {
-//       dispatch({
-//           type: "deposit",
-//           payload: amount
-//       });
-//   }
-// }
-
-
-// export const withdrawMoney = (amount) => {
-//   console.log(amount, "Lol")
-//   return (dispatch) => {
-//       dispatch({
-//           type: "withdraw",
-//           payload: amount
-//       });
-//   }
-// }
--- src/state/reducers/accountReducer.js
+++ src/state/reducers/accountReducer.js
@@ -1,17 +1,3 @@
-// const initalState = 0
-// const accountReducer = (state = initalState, action) => {
-//   switch (action.type) {
-//     case "deposit":
-//       return state + action.payload;
-//     case "withdraw":
-//       return state - action.payload;
-//     default:
-//       return state;
-//   }
-// };
-
-// export default accountReducer;
-
 const initialState = 0;
 
 const reducer = (state = initialState, action) => {
